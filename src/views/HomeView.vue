6b737d0f673cbede7a71913d813304b9857bc238
Vero-Sampedro <verosampedro@icloud.com>
2024-02-02 15:18:05 +0100

    cards design done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+import PastEventsUser from '@/components/PastEventsUser.vue';
+import UserCards from '@/components/UserCards.vue';
 
 </script>
 
 <template>
   <main>
   <h1>Holaaaa</h1>
+
+  <UserCards />
+  <PastEventsUser />
   </main>
 </template>
