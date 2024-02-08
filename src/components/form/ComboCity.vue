8e21a57521e99232ca4eae9a30daf51ec135534a
Alicia Gonzalez Castro <aliglezcastro@gmail.com>
2024-02-08 15:31:45 +0100

    combobox component rendered

--- src/components/form/ComboCity.vue
+++ src/components/form/ComboCity.vue
@@ -22,12 +22,39 @@ fetchCities();
 
 </script>
 <template>
-  <div>
-    <select v-model="fetchCities" multiple>
-      <option v-for="cities in allCities" v-bind:value="cities.city_name" :key="cities.id_city">
+  <div class="combFrm">
+    <label for="localidades" id="localidad">Localidades</label>
+    <select v-model="fetchCities" id="comboForm">
+      <option value="null">Seleccione localidad</option>
+      <option v-for="cities in allCities" v-bind:value="cities.city_name" :key="cities.id_city" selected>
         {{ cities.city_name }}
       </option>
     </select>
   </div>
 </template>
-<style scoped lang="scss"></style>
+
+<style scoped lang="scss">
+
+#localidad {
+  padding-left: 1%;
+  font-size: 18px;
+  font-weight: bold;
+}
+.combFrm {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 1%;
+  outline: 0;
+  color: $astur-blue;
+  font-family: $first-font;
+}
+
+select {
+  background-color: $astur-blue;
+  width: 400px;
+  border-radius: 10px;
+  border: solid 1;
+  color: white;
+}
+</style>
