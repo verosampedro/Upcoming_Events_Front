e49962e138816b9f06e8cce9a00815c1a8dafbe7
Alicia González Castro <aliglezcastro@gmail.com>
2024-02-08 08:01:42 +0100

    component combo city created

---
+++ src/components/form/ComboCity.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+
+import axios from 'axios';
+import { ref } from 'vue';
+
+interface cities {
+    id_city : number;
+    city_name: string;
+}
+
+const allCities = ref<cities[]>([]);
+
+const fetchCities = async () => {
+    const response = await axios.get("http://localhost:8080/api/v1/cities");
+    allCities.value = response.data;
+    console.log(response.data);
+}
+
+fetchCities();
+
+
+</script>
+<template>
+  <div>
+    <select v-model="fetchCities" multiple>
+      <option v-for="cities in allCities" v-bind:value="cities.city_name" :key="cities.id_city">
+        {{ cities.city_name }}
+      </option>
+    </select>
+  </div>
+</template>
+<style scoped lang="scss"></style>
--- src/views/UserDashboard.vue
+++ src/views/UserDashboard.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import ComboCity from '@/components/form/ComboCity.vue';
 </script>
 <template>
     <div>
-        
+        <ComboCity/>
     </div>
 </template>
 <style scoped lang="scss">
