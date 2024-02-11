3bec09d542c88af1b1deb591cbe29d40798096fc
Alicia González Castro <aliglezcastro@gmail.com>
2024-02-11 17:35:01 +0100

    dating component  created

---
+++ src/components/form/TextLabelComponent.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="dateContainer">
+    <div id="dates">
+      <label for="date" class="form-label">Fecha de inicio:</label>
+      <input type="date" class="form-control" id="date" />
+    </div>
+    <div class="dates">
+      <label for="date" class="form-label">Fecha fin:</label>
+      <input type="date" class="form-control" id="date" />
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+.dateContainer {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 1%;
+  gap: 10px;
+  font-family: $first-font;
+
+  .form-control   {
+    width: 200px;
+    border-radius:10px;
+    border: solid 1;
+    color: $astur-blue;
+    margin: 1%;
+  }
+  .form-label {
+    color: $astur-blue;
+    font-size: 18px;
+    font-weight: bold;
+    padding-left: 1%;
+  }
+}
+</style>
--- src/views/UserDashboard.vue
+++ src/views/UserDashboard.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import ComboCity from '@/components/form/ComboCity.vue';
+import TextLabelComponent from '@/components/form/TextLabelComponent.vue';
 </script>
 <template>
     <div>
         <ComboCity/>
+        <TextLabelComponent/>
     </div>
 </template>
 <style scoped lang="scss">
