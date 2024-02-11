2769204d10adebaebf0377f55d8de118264ae11a
Alicia González Castro <aliglezcastro@gmail.com>
2024-02-11 17:46:00 +0100

    renamed datescomponent

--- src/views/UserDashboard.vue
+++ src/views/UserDashboard.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import ComboCity from '@/components/form/ComboCity.vue';
-import TextLabelComponent from '@/components/form/TextLabelComponent.vue';
+import DatesComponent from '@/components/form/DatesComponent.vue';
 </script>
 <template>
     <div>
         <ComboCity/>
-        <TextLabelComponent/>
+        <DatesComponent/>
+        
     </div>
 </template>
 <style scoped lang="scss">
